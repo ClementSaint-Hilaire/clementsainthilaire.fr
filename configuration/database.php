fb7f40734babefa3a284c08ccb251af9452dbaa9
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-04-13 15:16:17 +0200

    navbar update

--- configuration/database.php
+++ configuration/database.php
@@ -6,3 +6,12 @@
         die('Erreur de connexion à la bdd : ' . $e->getMessage());
     }
 ?>
+
+<?php       
+    try {
+        $db = new PDO('mysql:host=clemezalibrecour.mysql.db;dbname=clemezalibrecour;charset=utf8;', 'clemezalibrecour', 'HZyvey267Ehce');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die('Erreur de connexion à la bdd : ' . $e->getMessage());
+    }
+?>
